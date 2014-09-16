80674904cdede0789e5da79994ac6a49817f5a0a
Jacob Fentress <jake@guerillalabs.co>
2014-09-16 16:55:09 -0500

    Contact menu icons. Closes #33. Closes #40.

--- partials/nav-site.php
+++ partials/nav-site.php
@@ -5,7 +5,7 @@
         <a class="site-nav__home" href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-white.svg" alt="MintLink"></a>
 
         <ul class="site-nav__list" role="navigation">
-          <li class="site-nav__item sm--hide"><a class="site-nav__link" href="#">Member Login<img class="site-nav__icon" src="<?php echo get_template_directory_uri(); ?>/img/membership-login.svg"></a></li>
+          <li class="site-nav__item sm--hide"><a class="site-nav__link" href="https://www.mymerchantdata.com/6_ms_login2.aspx">Member Login<img class="site-nav__icon" src="<?php echo get_template_directory_uri(); ?>/img/membership-login.svg"></a></li>
           <li class="site-nav__item">
             <a class="site-nav__link js-nav" href="#">Menu
               <div class="site-nav__button js-nav-btn">
